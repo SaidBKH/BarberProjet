9e2276db218e341c36755c7db557fd9fc33d14a0
SaidBKH <boukhdimisaid@gmail.com>
2024-04-22 15:00:10 +0200

    suite homePage

--- view/accueil/home.php
+++ view/accueil/home.php
@@ -6,13 +6,41 @@
     </div>
 
     <div class= accueil-presentation>
-    <figure><img class="devanture" src="public/img/devanture.jpeg"><img>
+        <figure><img class="devanture" src="public/img/devanture.jpeg"><img>
+        </figure>
+            <div class="texte-presentation">
+            <h2> Salon de Coiffure à Annemasse !</h2>
+            <br>
+            <p>Jesuispasséchezsouf est la concrétisation d'un concept du barber idéal perçu par son créateur, un lieu où l'on revient sans hésiter ! 
+            Imaginer comme une thérapie de bien être, ou l'on vient vivre un moment régénérateur et repartir avec d'avantage d'assurance et un style des plus adapté à sa personnalité.</p>
+            </div>
+    </div>
+</div>
+
+<div class ="list-services">
+    
+    <figure>
+        <img class="services" src="public/img/nos_services1.jpeg"><img>
     </figure>
-    <div class="texte-presentation">
-    <h2> Salon de Coiffure à Annemasse !</h2>
-    <br>
-    <p>Jesuispasséchezsouf est la concrétisation d'un concept du barber idéal perçu par son créateur, un lieu où l'on revient sans hésiter ! 
-    Imaginer comme une thérapie de bien être, ou l'on vient vivre un moment régénérateur et repartir avec d'avantage d'assurance et un style des plus adapté à sa personnalité.</p>
-    </div>
-    </div>
+    <figure>
+        <img class="services" src="public/img/nos_services2.jpeg"><img>
+    </figure>
+    <figure>
+        <img class="services" src="public/img/nos_services3.jpeg"><img>
+    </figure>
+    <figure>
+        <img class="services" src="public/img/services3.jpeg"><img>
+    </figure>
+
+<div class="instagram">
+
+</div>
+
+<div class="galerie">
+</div>
+
+<div class="newletter">
+</div>
+
+<div class="footer">
 </div>
