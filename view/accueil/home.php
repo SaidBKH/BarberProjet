b9125442ffe2d60b200ecc035857d1a74c06c173
SaidBKH <boukhdimisaid@gmail.com>
2024-04-23 18:34:59 +0200

    CSS homepage

--- view/accueil/home.php
+++ view/accueil/home.php
@@ -5,36 +5,66 @@
         <button class="btn-rdv">Prendre rendez-vous</button>
     </div>
 
-    <div class= accueil-presentation>
-        <figure><img class="devanture" src="public/img/devanture.jpeg"><img>
+    <div class="accueil-presentation">
+        <figure>
+            <img class="devanture" src="public/img/devanture.jpeg" alt="Devanture">
         </figure>
-            <div class="texte-presentation">
-            <h2> Salon de Coiffure à Annemasse !</h2>
+        <div class="texte-presentation">
+            <h2>Salon de Coiffure à Annemasse !</h2>
             <br>
-            <p>Jesuispasséchezsouf est la concrétisation d'un concept du barber idéal perçu par son créateur, un lieu où l'on revient sans hésiter ! 
-            Imaginer comme une thérapie de bien être, ou l'on vient vivre un moment régénérateur et repartir avec d'avantage d'assurance et un style des plus adapté à sa personnalité.</p>
-            </div>
+            <p>Jesuispasséchezsouf est la concrétisation d'un concept du barber idéal perçu par son créateur, un lieu où l'on revient sans hésiter !
+                Imaginer comme une thérapie de bien être, ou l'on vient vivre un moment régénérateur et repartir avec d'avantage d'assurance et un style des plus adapté à sa personnalité.</p>
+        </div>
     </div>
+
+    <div class="list-services">
+        <figure>
+            <a href="lien-service1" class="service-lien">
+                <img class="services" src="public/img/nos_services1.jpeg" alt="Service 1">
+                <div class="image-overlay">
+                    <p class="overlay-text">CHEVEUX</p>
+                </div>
+            </a>
+        </figure>
+        <figure>
+            <a href="lien-service2" class="service-lien">
+                <img class="services" src="public/img/nos_services2.jpeg" alt="Service 2">
+                <div class="image-overlay">
+                    <p class="overlay-text">BARBE</p>
+                </div>
+            </a>
+        </figure>
+        <figure>
+            <a href="lien-service3" class="service-lien">
+                <img class="services" src="public/img/nos_services3.jpeg" alt="Service 3">
+                <div class="image-overlay">
+                    <p class="overlay-text">SOINS</p>
+                </div>
+            </a>
+        </figure>
+        <figure>
+            <a href="lien-service4" class="service-lien">
+                <img class="services" src="public/img/image3.jpeg" alt="Service 4">
+                <div class="image-overlay">
+                    <p class="overlay-text">COULEUR</p>
+                </div>
+            </a>
+        </figure>
+    </div>
+
 </div>
 
-<div class ="list-services">
-    
-    <figure>
-        <img class="services" src="public/img/nos_services1.jpeg"><img>
-    </figure>
-    <figure>
-        <img class="services" src="public/img/nos_services2.jpeg"><img>
-    </figure>
-    <figure>
-        <img class="services" src="public/img/nos_services3.jpeg"><img>
-    </figure>
-    <figure>
-        <img class="services" src="public/img/services3.jpeg"><img>
-    </figure>
+    <div class="instagram">
+        <div class="instagram-contenu">
+            <h2>Suivez-nous sur Instagram</h2>
+            <p>Découvrez nos dernières créations et inspirations en nous suivant sur Instagram.</p>
+            <a href="https://www.instagram.com/jesuispassechezsouf/" class="instagram-lien">
+                <i class="fab fa-instagram"></i>
+                <span>@jesuispassechezsouf</span>
+            </a>
+        </div>
+    </div>
 
-<div class="instagram">
-
-</div>
 
 <div class="galerie">
 </div>
