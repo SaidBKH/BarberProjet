9ba7ae9c5594bf8a6dc9b5c3851d271f9fb36289
SaidBKH <boukhdimisaid@gmail.com>
2024-04-30 22:22:49 +0200

    style nous rejoindre

--- view/rejoindre/nous_rejoindre.php
+++ view/rejoindre/nous_rejoindre.php
@@ -1,20 +1,28 @@
-<h1>nous rejoindre</h1>
+<div class="container">
+    <h1>Nous Rejoindre</h1>
 
-<p>Rejoignez Notre Équipe de Barbiers
-Êtes-vous passionné par l'art de la coiffure et du rasage ? Rejoignez notre équipe dynamique et contribuez à offrir des expériences exceptionnelles à nos clients. Nous recherchons des barbiers talentueux et créatifs pour compléter notre équipe.
+    <div class="join-container">
+        <p>Rejoignez Notre Équipe de Barbiers</p>
+        <p>Êtes-vous passionné par l'art de la coiffure et du rasage ? Rejoignez notre équipe dynamique et contribuez à offrir des expériences exceptionnelles à nos clients. Nous recherchons des barbiers talentueux et créatifs pour compléter notre équipe.</p>
 
-Pourquoi Nous Rejoindre ?
-En tant que barbier chez Jesuispassechezsouf, vous aurez l'opportunité de mettre en valeur vos compétences et votre créativité. Voici ce que nous offrons :
+        <h2>Pourquoi Nous Rejoindre ?</h2>
+        <ul>
+            <li>Un Environnement Professionnel : Nous offrons un espace de travail moderne et équipé des derniers outils et produits de qualité.</li>
+            <li>Une Clientèle Établie : Rejoignez une équipe qui bénéficie d'une clientèle fidèle et croissante, ce qui vous permettra de développer votre carrière et de faire évoluer vos compétences.</li>
+            <li>Culture d'Équipe : Nous valorisons le travail d'équipe et l'échange de connaissances. Vous ferez partie d'une équipe collaborative qui encourage l'apprentissage continu et le développement personnel.</li>
+        </ul>
 
-Un Environnement Professionnel : Nous offrons un espace de travail moderne et équipé des derniers outils et produits de qualité.
-Une Clientèle Établie : Rejoignez une équipe qui bénéficie d'une clientèle fidèle et croissante, ce qui vous permettra de développer votre carrière et de faire évoluer vos compétences.
-Culture d'Équipe : Nous valorisons le travail d'équipe et l'échange de connaissances. Vous ferez partie d'une équipe collaborative qui encourage l'apprentissage continu et le développement personnel.
-Profil Recherché :
-Expérience confirmée en tant que barbier, avec de solides compétences en coiffure et en rasage.
-Capacité à offrir un service client exceptionnel et à créer des styles personnalisés en fonction des besoins de chaque client.
-Passion pour l'industrie de la coiffure et désir d'apprendre et de se perfectionner en permanence.
-Capacité à travailler efficacement dans un environnement dynamique et à gérer un flux de clients varié.
-Comment Postuler :
-Si vous êtes intéressé à rejoindre notre équipe passionnée, veuillez envoyer votre CV  à jesuispassechezsouf@gmail.com . Nous avons hâte d'en savoir plus sur vous et votre passion pour la coiffure.
+        <h2>Profil Recherché :</h2>
+        <ul>
+            <li>Expérience confirmée en tant que barbier, avec de solides compétences en coiffure et en rasage.</li>
+            <li>Capacité à offrir un service client exceptionnel et à créer des styles personnalisés en fonction des besoins de chaque client.</li>
+            <li>Passion pour l'industrie de la coiffure et désir d'apprendre et de se perfectionner en permanence.</li>
+            <li>Capacité à travailler efficacement dans un environnement dynamique et à gérer un flux de clients varié.</li>
+        </ul>
 
-Rejoignez-nous pour une aventure enrichissante dans le monde captivant de la coiffure et du rasage chez Jesuispassechezsouf.</p>
+        <h2>Comment Postuler :</h2>
+        <p>Si vous êtes intéressé à rejoindre notre équipe passionnée, veuillez envoyer votre CV à <a href="mailto:jesuispassechezsouf@gmail.com">jesuispassechezsouf@gmail.com</a>. Nous avons hâte d'en savoir plus sur vous et votre passion pour la coiffure.</p>
+
+        <p>Rejoignez-nous pour une aventure enrichissante dans le monde captivant de la coiffure et du rasage chez Jesuispassechezsouf.</p>
+    </div>
+</div>
